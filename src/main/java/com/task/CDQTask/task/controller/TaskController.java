4f4bd0f2dac98779d1d7c93d374c8c4e5c1ade75
Patryk <chojnackipatryk96@gmail.com>
2022-04-08 14:54:32 +0200

    Implement Task Controller

--- src/main/java/com/task/CDQTask/task/controller/TaskController.java
+++ src/main/java/com/task/CDQTask/task/controller/TaskController.java
@@ -1,5 +1,12 @@
 package com.task.CDQTask.task.controller;
 
+import com.task.CDQTask.task.dto.TaskDto;
+import com.task.CDQTask.task.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/tasks")
 public class TaskController {
 
+    public final TaskService taskService;
 
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity createTask(@RequestBody TaskDto task){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity readTask(@PathVariable Long id){
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity readAllTasks(@PathVariable Long id){
+        return ResponseEntity.ok().build();
+    }
 }
